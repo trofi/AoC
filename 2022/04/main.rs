f3c99d560bd58d802df6e635deb4162ab56b6432
Sergei Trofimovich <slyich@gmail.com>
2022-12-04 11:55:26 +0000

    2022: 04: first attempt at returning useful errors

--- 2022/04/main.rs
+++ 2022/04/main.rs
@@ -5,6 +5,9 @@ use std::str::FromStr;
 use std::error::Error;
 
 type E = Box<dyn Error>;
+fn mk_e(err: String) -> E {
+  Box::<dyn Error>::from(err)
+}
 
 #[derive(Debug, Clone, PartialEq)]
 struct Assignment { from: usize, to: usize, }
@@ -22,15 +25,15 @@ impl Assignment {
 }
 
 impl FromStr for Assignment {
-  type Err = ();
+  type Err = E;
   fn from_str(s: &str) -> Result<Self, Self::Err> {
-    let numbers: Vec<usize> =
+    let numbers: Result<Vec<usize>, Self::Err> =
       s.split('-').map(|e|
-        e.parse().expect("a number")
+        e.parse().map_err(|err| mk_e(format!("'{}': {}", e, err)))
       ).collect();
-    match numbers.as_slice() {
+    match numbers?.as_slice() {
       &[a, b] => Ok(Assignment{from: a, to: b}),
-      _       => Err(()),
+      _       => Err(mk_e(format!("'{}' does not match N-M format", s)))
     }
   }
 }
@@ -41,43 +44,43 @@ struct Pair {
 }
 
 impl FromStr for Pair {
-  type Err = ();
+  type Err = E;
   fn from_str(s: &str) -> Result<Self, Self::Err> {
-    let segs: Vec<Assignment> = s.split(',').map(|seg|
-      seg.parse().expect("N-M format")
+    let segs: Result<Vec<Assignment>, Self::Err> = s.split(',').map(|seg|
+      seg.parse().map_err(|e: <Assignment as FromStr>::Err| e.into())
     ).collect();
-    Ok(Pair{segments: segs})
+    Ok(Pair{segments: segs?})
   }
 }
 
-fn solve_p1(input: &Vec<&str>) -> Result<usize, ()> {
-  let r = input.iter().map(|l| {
-      let p: Pair = l.parse().expect("pair format did not match");
+fn solve_p1(input: &Vec<&str>) -> Result<usize, E> {
+  let r: Result<Vec<usize>, E> = input.iter().map(|l| {
+      let p: Pair = l.parse()?;
       let i = p.segments
                .iter()
                .cloned()
                .reduce(|a, v| a.intersect(&v))
-               .expect("at least one element");
+               .map_or(Err(mk_e(format!("{} ls empty", l))), Ok)?;
 
-      if p.segments.contains(&i) { 1 } else { 0 }
+      Ok(if p.segments.contains(&i) { 1 } else { 0 })
     }
-  ).sum();
-  Ok(r)
+  ).collect();
+  Ok(r?.into_iter().sum())
 }
 
-fn solve_p2(input: &Vec<&str>) -> Result<usize, ()> {
-  let r =  input.iter().map(|l| {
-      let p: Pair = l.parse().expect("pair format did not match");
+fn solve_p2(input: &Vec<&str>) -> Result<usize, E> {
+  let r: Result<Vec<usize>, E> =  input.iter().map(|l| {
+      let p: Pair = l.parse()?;
       let i = p.segments
                .iter()
                .cloned()
                .reduce(|a, v| a.intersect(&v))
-               .expect("at least one element");
+               .map_or(Err(mk_e(format!("{} ls empty", l))), Ok)?;
 
-      if i.is_empty() { 0 } else { 1 }
+      Ok(if i.is_empty() { 0 } else { 1 })
     }
-  ).sum();
-  Ok(r)
+  ).collect();
+  Ok(r?.into_iter().sum())
 }
 
 fn main() -> Result<(), E> {
@@ -85,9 +88,9 @@ fn main() -> Result<(), E> {
     let input_string = read_to_string(ifile)?;
     let input: Vec<&str> = input_string.lines().collect();
 
-    let ans_p1 = solve_p1(&input).expect("p1 failed");
+    let ans_p1 = solve_p1(&input)?;
     println!("{}: P1 ans: {:?}", ifile, ans_p1);
-    let ans_p2 = solve_p2(&input).expect("p2 failed");
+    let ans_p2 = solve_p2(&input)?;
     println!("{}: P2 ans: {:?}", ifile, ans_p2);
   }
   Ok(())
